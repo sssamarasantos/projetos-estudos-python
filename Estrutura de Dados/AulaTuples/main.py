00731f0882f6ac488918dc001fc19b4b037b49dd
Samara Santos <samara.souza2013@outlook.com>
2023-11-29 21:13:32 -0300

    estrutura de dados - tuples

---
+++ Estrutura de Dados/AulaTuples/main.py
@@ -0,0 +1,10 @@
+# Tuples
+  # -Listas imutavéis
+  # -Manter a sequência de dados
+  # -Armazenar mais de uma informação em uma variável
+
+cores = ('azul', 'rosa', 'preto')
+
+print(cores.index('rosa')) #1
+print(cores[2]) #preto
+print(cores[-3]) #azul
